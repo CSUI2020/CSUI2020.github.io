568bcb687680f1963982e1d672ba43bc91ee8526
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-11-12 20:26:26 +0700

    Deploy

---
+++ precache-manifest.39fc9d58647c7ffc2a889749bbc98061.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74a0cf6e2b66d5b44a4",
+    "url": "/csui2020/css/app.b6a3fb60.css"
+  },
+  {
+    "revision": "d54a80c5badc25a57008f29060dd73ac",
+    "url": "/csui2020/index.html"
+  },
+  {
+    "revision": "e74a0cf6e2b66d5b44a4",
+    "url": "/csui2020/js/app.dfe868d7.js"
+  },
+  {
+    "revision": "9f1d4ebd8451615a62bd",
+    "url": "/csui2020/js/chunk-vendors.e76ede10.js"
+  },
+  {
+    "revision": "77a263662158acf16c588396f24e18b8",
+    "url": "/csui2020/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/csui2020/robots.txt"
+  },
+  {
+    "revision": "81051bcc2cf1bedf378224b0a93e2877",
+    "url": "/csui2020/static/css/background.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/csui2020/static/css/typography.css"
+  },
+  {
+    "revision": "f07a2bb363a4ee55ab212f855e912668",
+    "url": "/csui2020/static/svg/Introduction Text.svg"
+  },
+  {
+    "revision": "4fad8f517425cbd406ce5d02760d6dc5",
+    "url": "/csui2020/static/svg/Profile Circle.svg"
+  },
+  {
+    "revision": "6e750e0c5a2d0d844b43ed249e8b82c4",
+    "url": "/csui2020/static/svg/Search Bar Default State.svg"
+  },
+  {
+    "revision": "7dc21c4273de171312508401c636e924",
+    "url": "/csui2020/static/svg/Search Bar Hover State.svg"
+  },
+  {
+    "revision": "42debde73f1f65fee1ead0356ed007f3",
+    "url": "/csui2020/static/svg/background-1.svg"
+  },
+  {
+    "revision": "41372529a084ad4d000ee60c6ee1dc0a",
+    "url": "/csui2020/static/svg/background-2.svg"
+  },
+  {
+    "revision": "7e90f931e4b688568fc2d7ba998aebed",
+    "url": "/csui2020/static/svg/background-3.svg"
+  },
+  {
+    "revision": "da7482baaf478a68c7c6295890770f1c",
+    "url": "/csui2020/static/svg/loading.webp"
+  },
+  {
+    "revision": "c97fedd4c27582d6464cad79c2f77f34",
+    "url": "/csui2020/static/svg/logo.svg"
+  }
+]);
