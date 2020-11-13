bdb3ccb3d603f238241d6f20f21f1f9c2c6978aa
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-11-13 23:10:52 +0700

    Deploy

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/#/precache-manifest.a516736ac16a3fe0b43863175dc54006.js"
+  "/precache-manifest.43991b1f1232deccb640c944d7f8b8f7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "website-angkatan"});
